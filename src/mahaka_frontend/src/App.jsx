2c8a306c02787569a342db62f36f5d9da1a8c970
aarchijainquadb <aarchi.jain@quadbtech.com>
2024-09-03 12:51:05 +0530

    event reducer

--- src/mahaka_frontend/src/App.jsx
+++ src/mahaka_frontend/src/App.jsx
@@ -1,51 +1,120 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import appRoutes from "./routes/app.routing";
-// import { useDispatch, useSelector } from 'react-redux';
-// import { createVenue } from '../src/redux/reducers/apiReducers/venueApiReducer';
+// import { useSelector, useDispatch } from "react-redux";
+// import { Principal } from "@dfinity/principal";
+// import { createEvent, getAllEventsByVenue } from "../src/redux/reducers/apiReducers/eventApiReducer";
 
 function App() {
   // const dispatch = useDispatch();
-  // const { backend } = useSelector(state => state.auth);
+  // const { backend } = useSelector((state) => state.auth);
 
-  // const handleCreateVenue = () => {
-  //   dispatch(createVenue({
-  //     backend,
-  //     collectionArgs: {
-  //       maxLimit: 100,
-  //       logo: {
-  //         data: "example data",
-  //         logo_type: "image"
-  //       },
-  //       name: "Venue Name",
-  //       banner: {
-  //         data: "example data",
-  //         logo_type: "image"
-  //       },
-  //       description: "description of the venue.",
-  //       created_at: 123456,
-  //       symbol: "venue_symbol"
-  //     },
-  //     custodian: "2vxsx-fae",
-  //     title: "Venue Title",
-  //     capacity: 500,
-  //     details: {
-  //       StartDate: "2024-09-01",
-  //       StartTime: 14,
-  //       Location: "Example Location",
-  //       EndDate: "2024-09-01", 
-  //       EndTime: 16,
-  //     },
-  //     description: "description of the venue"
-  //   }));
+  // const createEventHandler = async () => {
+  //    dispatch(createEvent({
+  //         backend,
+  //         venueId: "Venue Title-br5f7-7uaaa-aaaaa-qaaca-cai",
+  //         Event: {
+  //           Description: "testing",
+  //           GroupTicket: 20,
+  //           Details: {
+  //             StartDate: "2024-09-01",
+  //             StartTime: 14,
+  //             Location: "Example Location",
+  //             EndDate: "2024-09-01",
+  //             EndTime: 1,
+  //           },
+  //           SingleTicket: 50,
+  //           Title: "testing",
+  //           VipTicket: 50,
+  //         },
+  //         eCollection: {
+  //           collection_args: {
+  //             maxLimit: 1000,
+  //             logo: {
+  //               data: "example data",
+  //               logo_type: "image/png",
+  //             },
+  //             name: "Event Collection Name",
+  //             banner: {
+  //               data: "example data",
+  //               logo_type: "image",
+  //             },
+  //             description: "This is a description",
+  //             created_at: 1234,
+  //             collection_type: { Event: null },
+  //             symbol: "symbol",
+  //           },
+  //         },
+  //       })
+  //     );
+  //     console.log("Event created successfully");
   // };
 
-  // const { theme, toggleTheme } = useTheme();
+  // const fetchEventsHandler = async () => {
+  //   dispatch(getAllEventsByVenue({
+  //         backend,
+  //         chunkSize: 10,
+  //         pageNo: 1, 
+  //         venueId: "Venue Title-br5f7-7uaaa-aaaaa-qaaca-cai", 
+  //       })
+  //     );
+  //     console.log("Events fetched successfully");
+   
+  // };
 
+  // const createVenueHandler = async () => {
+  //   try {
+  //     const response = await backend.createVenue(
+  //       {
+  //         collection_args: {
+  //           maxLimit: 100,
+  //           logo: {
+  //             data: "example data",
+  //             logo_type: "image",
+  //           },
+  //           name: "Venue Name",
+  //           banner: {
+  //             data: "example data",
+  //             logo_type: "image",
+  //           },
+  //           description: "description of the venue.",
+  //           created_at: 123456,
+  //           collection_type: { Venue: null },
+  //           symbol: "venue_symbol",
+  //         },
+  //         custodian: Principal.fromText("2vxsx-fae"),
+  //       },
+  //       "Venue Title",
+  //       500,
+  //       {
+  //         StartDate: "2024-09-01",
+  //         StartTime: 14,
+  //         Location: "Example Location",
+  //         EndDate: "2024-09-01",
+  //         EndTime: 16,
+  //       },
+  //       "description of the venue"
+  //     );
+
+  //     console.log("create venue result", response);
+  //   } catch (err) {
+  //     console.error("Error in create venue", err);
+  //   }
+  // };
+
+    // const { theme, toggleTheme } = useTheme();
   return (
     <div className="light">
-      {/* <button onClick={handleCreateVenue}>Create Venue</button> */}
-      {/* <div className="p-4">
+      {/* <button className="px-2 py-1 border rounded mr-2" onClick={createEventHandler}>
+        Create Event
+      </button>
+      <button className="px-2 py-1 border rounded mr-2" onClick={createVenueHandler}>
+        Create Venue
+      </button>
+      <button className="px-2 py-1 border rounded" onClick={fetchEventsHandler}>
+        Fetch Events
+      </button> */}
+ {/* <div className="p-4">
       <div className="bg-foreground p-4 rounded-2xl">I am {theme} theme.</div>
       <button
         onClick={toggleTheme}
@@ -54,7 +123,7 @@ function App() {
         Toggle Theme
       </button>
     </div>     */}
-      <RouterProvider router={appRoutes} /> 
+      <RouterProvider router={appRoutes} />
     </div>
   );
 }
--- src/mahaka_frontend/src/redux/store.js
+++ src/mahaka_frontend/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/auth/authReducer";
 import venueApiReducer from "./reducers/apiReducers/venueApiReducer";
 import userApiReducer from "./reducers/apiReducers/userApiReducer";
+import eventApiReducer from "./reducers/apiReducers/eventApiReducer";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     venues: venueApiReducer,
     users: userApiReducer,
+    events: eventApiReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
