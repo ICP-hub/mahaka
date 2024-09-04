cd350305dca665c95c3f44610c776b02edfde93e
ankurnayak1 <ankur.nayak@quadbtech.com>
2024-09-04 18:15:43 +0530

    update_overlay_modal

--- src/mahaka_frontend/src/admin/layout/navigation/AppBar.jsx
+++ src/mahaka_frontend/src/admin/layout/navigation/AppBar.jsx
@@ -3,7 +3,7 @@ import ButtonWrapper from "../../../common/ButtonWrapper";
 
 const AppBar = ({ toggleNavigation }) => {
   return (
-    <div className="bg-card relative z-49 flex h-16 w-full flex-0 items-center px-4 shadow dark:border-b dark:bg-transparent dark:shadow-none md:px-6 print:hidden">
+    <div className="bg-card relative z-49 flex h-16 w-full flex-0 items-center px-4 shadow dark:border-b dark:bg-transparent dark:shadow-none md:px-6 border-border">
       <ButtonWrapper
         size={24}
         color="text-icon hover:text-secondary"
--- src/mahaka_frontend/src/customer/Components/Modal-overlay.jsx
+++ src/mahaka_frontend/src/customer/Components/Modal-overlay.jsx
@@ -9,23 +9,26 @@ const ModalOverlay = ({ isOpen, setIsOpen, title, children }) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          onClick={() => setIsOpen(false)}
-          className="bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-999 grid place-items-center overflow-y-scroll cursor-pointer"
+          className="bg-slate-900/20 backdrop-blur p-0 md:p-8 fixed inset-0 z-999 grid place-items-center overflow-y-scroll cursor-pointer"
         >
           <motion.div
             initial={{ scale: 0, rotate: "12.5deg" }}
             animate={{ scale: 1, rotate: "0deg" }}
             exit={{ scale: 0, rotate: "0deg" }}
             onClick={(e) => e.stopPropagation()}
-            className="rounded-2xl w-full max-w-lg cursor-default relative overflow-hidden bg-white shadow-lg"
+            className="md:rounded-2xl w-full max-w-xl cursor-default relative overflow-hidden bg-white shadow-lg"
           >
-            <div className="flex justify-between px-8 py-4 items-center bg-gray-100 text-gray-600">
-              <h3 className="text-xl font-semibold text-center">{title}</h3>
-              <button onClick={() => setIsOpen(false)}>
+            <div className="flex justify-between px-8 py-4 items-center bg-secondary text-white">
+              <h3 className="text-xl font-semibold text-center flex-1">
+                {title}
+              </h3>
+              <button onClick={() => setIsOpen(false)} className="ml-4">
                 <MdClose size={16} />
               </button>
             </div>
-            <div className="relative z-10 p-6">{children}</div>
+            <div className="relative z-10 p-6 max-h-[90vh] md:max-h-[80vh] overflow-y-auto ">
+              {children}
+            </div>
           </motion.div>
         </motion.div>
       )}
