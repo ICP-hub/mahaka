6208103017f52113dff9e41b8dee3d09e8c697be
aarchijainquadb <aarchi.jain@quadbtech.com>
2024-09-05 12:43:46 +0530

    create venue form

--- src/mahaka_frontend/src/admin/components/CreateVenueForm.jsx
+++ src/mahaka_frontend/src/admin/components/CreateVenueForm.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createVenue } from "../../redux/reducers/apiReducers/venueApiReducer";
+
 const CreateVenueForm = () => {
+  const dispatch = useDispatch();
+  const { backend } = useSelector((state) => state.auth);
   const [venueData, setVenueData] = useState({
     collection_args: {
-      maxLimit: 100, 
-      logo: { 
-        data: "", 
-        logo_type: "" 
+      maxLimit: 100,
+      logo: {
+        data: "logodata_static",
+        logo_type: "logotype_static",
       },
       name: "",
-      banner: { 
-        data: "", 
-        logo_type: "" 
+      banner: {
+        data: "",
+        logo_type: "",
       },
       description: "",
       created_at: Date.now(),
-      collection_type: "Venue", 
-      symbol: "",
+      collection_type: { Venue: null },
+      symbol: "Venue_symbol_static",
     },
-    custodian: "", 
+    custodian: "2vxsx-fae",
     title: "",
-    price: 0,
+    capacity: 0,
     eventDetails: {
       StartDate: "",
       StartTime: "",
@@ -29,9 +34,7 @@ const CreateVenueForm = () => {
     },
   });
 
-  const handleFileChange = (e) => {
-    setVenueData({ ...venueData, logo_type: e.target.files[0] });
-  };
+  const [bannerPreview, setBannerPreview] = useState("");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -52,140 +55,221 @@ const CreateVenueForm = () => {
     }));
   };
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const fileType = file.type;
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setVenueData((prevState) => ({
+          ...prevState,
+          collection_args: {
+            ...prevState.collection_args,
+            banner: {
+              data: reader.result,
+              logo_type: fileType,
+            },
+          },
+        }));
+        setBannerPreview(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  // Handler form submit
+  const handleVenueSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      createVenue({
+        backend,
+        collectionArgs: venueData.collection_args,
+        custodian: "2vxsx-fae",
+        title: venueData.title,
+        capacity: parseInt(venueData.capacity),
+        details: { ...venueData.eventDetails, StartTime: 4, EndTime: 6 },
+        description: "description of the venue",
+      })
+    );
+  };
+
   return (
-  <>
-  <form  className="space-y-4">
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Venue Name</label>
-          <input
-            type="text"
-            name="name"
-            value={venueData.collection_args.name}
-            onChange={(e) => handleNestedInputChange(e, "collection_args", "name")}
-            className="mt-1 p-2 w-full border rounded border-gray-300"
-            placeholder="Venue name"
-            required
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Description</label>
-          <textarea
-            name="description"
-            value={venueData.collection_args.description}
-            onChange={(e) => handleNestedInputChange(e, "collection_args", "description")}
-            className="mt-1 p-2 w-full border rounded border-gray-300"
-            placeholder="Venue description"
-            required
-          />
-        </div>
-        <div className="flex space-x-4">
-          <div className="w-1/2">
-            <label className="block text-sm font-medium text-gray-700">Start Date</label>
+    <>
+      <form className="space-y-2" onSubmit={handleVenueSubmit}>
+        <div className="flex flex-col flex-auto gap-1">
+          <label className="font-semibold">Venue Name</label>
+          <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
             <input
-              type="date"
-              name="StartDate"
-              value={venueData.eventDetails.StartDate}
-              onChange={(e) => handleNestedInputChange(e, "eventDetails", "StartDate")}
-              className="mt-1 p-2 w-full border rounded border-gray-300"
+              type="text"
+              name="name"
+              value={venueData.collection_args.name}
+              onChange={(e) =>
+                handleNestedInputChange(e, "collection_args", "name")
+              }
+              className="my-3 outline-none w-full bg-none"
               required
             />
           </div>
-          <div className="w-1/2">
-            <label className="block text-sm font-medium text-gray-700">End Date</label>
-            <input
-              type="date"
-              name="EndDate"
-              value={venueData.eventDetails.EndDate}
-              onChange={(e) => handleNestedInputChange(e, "eventDetails", "EndDate")}
-              className="mt-1 p-2 w-full border rounded border-gray-300"
+        </div>
+        <div className="flex flex-col flex-auto gap-1">
+          <label className="font-semibold">Description</label>
+          <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
+            <textarea
+              name="description"
+              value={venueData.collection_args.description}
+              onChange={(e) =>
+                handleNestedInputChange(e, "collection_args", "description")
+              }
+              className="my-3 outline-none w-full bg-none"
               required
             />
           </div>
         </div>
         <div className="flex space-x-4">
-          <div className="w-1/2">
-            <label className="block text-sm font-medium text-gray-700">Start Time</label>
+          <div className="w-1/2 flex flex-col flex-auto gap-1">
+            <label className="font-semibold">Start Date</label>
+            <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
+              <input
+                type="date"
+                name="StartDate"
+                value={venueData.eventDetails.StartDate}
+                onChange={(e) =>
+                  handleNestedInputChange(e, "eventDetails", "StartDate")
+                }
+                className="my-3 outline-none w-full bg-none"
+                required
+              />
+            </div>
+          </div>
+          <div className="w-1/2 flex flex-col flex-auto gap-1">
+            <label className="font-semibold">End Date</label>
+            <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
+              <input
+                type="date"
+                name="EndDate"
+                value={venueData.eventDetails.EndDate}
+                onChange={(e) =>
+                  handleNestedInputChange(e, "eventDetails", "EndDate")
+                }
+                className="my-3 outline-none w-full bg-none"
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex space-x-4">
+          <div className="w-1/2 flex flex-col flex-auto gap-1">
+            <label className="font-semibold">Start Time</label>
+            <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
+              <input
+                type="time"
+                name="StartTime"
+                value={venueData.eventDetails.StartTime}
+                onChange={(e) =>
+                  handleNestedInputChange(e, "eventDetails", "StartTime")
+                }
+                className="my-3 outline-none w-full bg-none"
+                required
+              />
+            </div>
+          </div>
+          <div className="w-1/2 flex flex-col flex-auto gap-1">
+            <label className="font-semibold">End Time</label>
+            <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
+              <input
+                type="time"
+                name="EndTime"
+                value={venueData.eventDetails.EndTime}
+                onChange={(e) =>
+                  handleNestedInputChange(e, "eventDetails", "EndTime")
+                }
+                className="my-3 outline-none w-full bg-none"
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col flex-auto gap-1">
+          <label className="font-semibold">Location</label>
+          <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
             <input
-              type="time"
-              name="StartTime"
-              value={venueData.eventDetails.StartTime}
-              onChange={(e) => handleNestedInputChange(e, "eventDetails", "StartTime")}
-              className="mt-1 p-2 w-full border rounded border-gray-300"
+              type="text"
+              name="Location"
+              value={venueData.eventDetails.Location}
+              onChange={(e) =>
+                handleNestedInputChange(e, "eventDetails", "Location")
+              }
+              className="my-3 outline-none w-full bg-none"
               required
             />
           </div>
-          <div className="w-1/2">
-            <label className="block text-sm font-medium text-gray-700">End Time</label>
+        </div>
+        <div className="flex flex-col flex-auto gap-1">
+          <label className="font-semibold">Title</label>
+          <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
             <input
-              type="time"
-              name="EndTime"
-              value={venueData.eventDetails.EndTime}
-              onChange={(e) => handleNestedInputChange(e, "eventDetails", "EndTime")}
-              className="mt-1 p-2 w-full border rounded border-gray-300"
+              type="text"
+              name="title"
+              value={venueData.title}
+              onChange={handleInputChange}
+              className="my-3 outline-none w-full bg-none"
               required
             />
           </div>
         </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Location</label>
-          <input
-            type="text"
-            name="Location"
-            value={venueData.eventDetails.Location}
-            onChange={(e) => handleNestedInputChange(e, "eventDetails", "Location")}
-            className="mt-1 p-2 w-full border rounded border-gray-300"
-            placeholder="Venue location"
-            required
-          />
+        <div className="flex flex-col flex-auto gap-1">
+          <label className="font-semibold">Maximum number of people</label>
+          <div className="border border-border rounded-lg px-4 focus-within:border-indigo-600">
+            <input
+              type="number"
+              name="capacity"
+              value={venueData.capacity}
+              onChange={handleInputChange}
+              className="my-3 outline-none w-full bg-none"
+              required
+            />
+          </div>
         </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Title</label>
-          <input
-            type="text"
-            name="title"
-            value={venueData.title}
-            onChange={handleInputChange}
-            className="mt-1 p-2 w-full border rounded border-gray-300"
-            placeholder="Event title"
-            required
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Price</label>
-          <input
-            type="number"
-            name="price"
-            value={venueData.price}
-            onChange={handleInputChange}
-            className="mt-1 p-2 w-full border rounded border-gray-300"
-            placeholder="Event price"
-            required
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">Logo</label>
-          <div className="mt-1 flex flex-col items-center justify-center border-dashed border-2 border-gray-300 p-4 rounded">
+        <div className="flex flex-col flex-auto gap-1">
+          <label className="font-semibold">Banner</label>
+          <div className="flex flex-col items-center justify-center border-dashed border-2 border-border p-4 rounded">
             <input
               type="file"
               accept=".jpg,.jpeg,.png"
-              onChange={handleFileChange}
               className="hidden"
               id="upload-image"
+              onChange={handleFileChange}
             />
             <label
               htmlFor="upload-image"
-              className="cursor-pointer bg-gray-100 py-2 px-4 rounded-md border border-gray-300"
+              className="cursor-pointer bg-gray-100 py-2 px-4 rounded-md border border-border"
             >
-              Upload Logo
+              Upload Banner
             </label>
-            <p className="text-sm text-gray-500 mt-2">JPEG, PNG less than 5MB</p>
+            <p className="text-sm text-gray-500 mt-2">
+              JPEG, PNG less than 5MB
+            </p>
+            {bannerPreview && (
+              <img
+                src={bannerPreview}
+                alt="Banner Preview"
+                className="mt-2 w-full h-auto rounded"
+              />
+            )}
           </div>
         </div>
         <div className="flex justify-center">
-          <button type="submit" className="bg-orange-500 text-white py-2 px-4 rounded">Create</button>
+          <button
+            type="submit"
+            className="bg-orange-500 text-white py-2 px-4 rounded"
+          >
+            Create Venue
+          </button>
         </div>
       </form>
-      </>
+    </>
   );
 };
 
