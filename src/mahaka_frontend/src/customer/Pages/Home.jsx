8678d40d4ce01a1c932b55c3db193d94acc08b11
avanishsrivastava2907 <avanish.ranjan@quadbtech.com>
2024-11-08 20:23:38 +0530

    removed_price

--- src/mahaka_frontend/src/customer/Pages/Home.jsx
+++ src/mahaka_frontend/src/customer/Pages/Home.jsx
@@ -34,7 +34,6 @@ const VenueCard = ({ venue, layout }) => (
         <div>
           <h3 className="text-3xl font-black mb-2">{venue.Title}</h3>
           <p className="mb-4 text-[14px] font-normal">{venue.Description}</p>
-          <p className="text-2xl font-black">Rp {venue.price}/-</p>
         </div>
         <div className={`${layout.iconPadding}`}>
           <GoArrowUpRight className="bg-white w-[30px] h-[30px] text-[#E2AF4E] rounded-full p-1" />
@@ -134,10 +133,10 @@ export default function Home() {
           <h2 className="text-5xl font-bold text-center mb-8">Our Venues</h2>
           {loading ? (
             <div className="rounded-lg m-2 md:flex   text-center">
-              <div className="w-full lg:w-1/2 h-90 m-2 bg-gray-400 animate-pulse rounded-lg"></div>
-              <div className="w-full lg:w-1/2 h-90 m-2 bg-gray-400 animate-pulse rounded-lg"></div>
+              <div className="w-full lg:w-1/2 h-120 m-2 bg-gray-400 animate-pulse rounded-lg"></div>
+              <div className="w-full lg:w-1/2 h-120 m-2 bg-gray-400 animate-pulse rounded-lg"></div>
 
-              <div className="w-full lg:w-1/2 h-90 m-2 bg-gray-400 animate-pulse rounded-lg"></div>
+              <div className="w-full lg:w-1/2 h-120 m-2 bg-gray-400 animate-pulse rounded-lg"></div>
             </div>
           ) : (
             <div>
