a23efe98941913dfe9c79b6dee4108e626c85ff6
aarchijainquadb <aarchi.jain@quadbtech.com>
2024-11-18 16:39:53 +0530

    event manager

--- src/mahaka_frontend/src/admin/components/navigation/MenuSection.jsx
+++ src/mahaka_frontend/src/admin/components/navigation/MenuSection.jsx
@@ -23,7 +23,7 @@ const MenuSection = () => {
       label: "Wahana",
       icon: <HiOutlineStar size={24} />,
     },
-    // { path: "/admin/events", label: "Events", icon: <MdEventNote size={24} /> },
+    { path: "/admin/events", label: "Events", icon: <MdEventNote size={24} /> },
     { path: "/admin/venues", label: "Venues", icon: <MdMap size={24} /> },
     { path: "/admin/members", label: "Members", icon: <MdPerson size={24} /> },
     { path: "/admin/sales", label: "Sales", icon: <MdEqualizer size={24} /> },
